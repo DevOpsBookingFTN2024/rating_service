c14b41a5f101ead08c79f5a953c80cbc9e7d9c85
Ruzica74 <ruzicaa997@gmail.com>
2025-01-26 21:14:34 +0100

    feat: Adding rating management
    Adding rating management

---
+++ src/main/java/uns/ac/rs/rating_service/controller/HostRatingController.java
@@ -0,0 +1,67 @@
+package uns.ac.rs.rating_service.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uns.ac.rs.rating_service.dto.HostRatingDTO;
+import uns.ac.rs.rating_service.dto.request.CreateHostRatingRequest;
+import uns.ac.rs.rating_service.dto.request.UpdateHostRatingRequest;
+import uns.ac.rs.rating_service.dto.response.MessageResponse;
+import uns.ac.rs.rating_service.service.HostRatingService;
+import java.util.List;
+import java.util.UUID;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/ratings/host")
+public class HostRatingController {
+    @Autowired
+    private HostRatingService hostRatingService;
+
+    @PostMapping("/create/{host}")
+    public ResponseEntity<?> rateHost(@PathVariable String host,
+                                      @Valid @RequestBody CreateHostRatingRequest createHostRatingRequest,
+                                      @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = hostRatingService.rateHost(
+                host, createHostRatingRequest, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @GetMapping("/all/{host}")
+    public ResponseEntity<?> getAllHostRatingsByHost(@PathVariable String host) {
+        List<HostRatingDTO> hostRatings = hostRatingService.getAllHostRatingsByHost(host);
+        return ResponseEntity.ok(hostRatings);
+    }
+
+    @GetMapping("/{hostRatingId}")
+    public ResponseEntity<?> getHostRatingById(@PathVariable UUID hostRatingId) {
+        HostRatingDTO hostRating = hostRatingService.getHostRatingById(hostRatingId);
+        return ResponseEntity.ok(hostRating);
+    }
+
+    @PutMapping("/update/{hostRatingId}")
+    public ResponseEntity<?> updateHostRating(@PathVariable UUID hostRatingId,
+                                              @Valid @RequestBody UpdateHostRatingRequest updateHostRatingRequest,
+                                              @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = hostRatingService.updateHostRating(
+                hostRatingId, updateHostRatingRequest, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @GetMapping("/average/{host}")
+    public ResponseEntity<?> getHostAverageRating(@PathVariable String host) {
+        Double averageRating = hostRatingService.getHostAverageRating(host);
+        return ResponseEntity.ok(averageRating);
+    }
+
+    @DeleteMapping("/delete/{hostRatingId}")
+    public ResponseEntity<?> deleteHostRating(@PathVariable UUID hostRatingId,
+                                              @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = hostRatingService.deleteHostRating(hostRatingId, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+}
---
+++ src/main/java/uns/ac/rs/rating_service/service/HostRatingService.java
@@ -0,0 +1,150 @@
+package uns.ac.rs.rating_service.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import uns.ac.rs.rating_service.dto.HostRatingDTO;
+import uns.ac.rs.rating_service.dto.UserDTO;
+import uns.ac.rs.rating_service.dto.request.CreateHostRatingRequest;
+import uns.ac.rs.rating_service.dto.request.UpdateHostRatingRequest;
+import uns.ac.rs.rating_service.dto.response.MessageResponse;
+import uns.ac.rs.rating_service.mapper.HostRatingMapper;
+import uns.ac.rs.rating_service.model.HostRating;
+import uns.ac.rs.rating_service.repository.HostRatingRepository;
+import uns.ac.rs.rating_service.service.client.ReservationServiceClient;
+import uns.ac.rs.rating_service.service.client.UserServiceClient;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class HostRatingService {
+    private final HostRatingRepository hostRatingRepository;
+    private final UserServiceClient userServiceClient;
+    private final ReservationServiceClient reservationServiceClient;
+
+    public HostRatingService(HostRatingRepository hostRatingRepository,
+                             UserServiceClient userServiceClient,
+                             ReservationServiceClient reservationServiceClient) {
+        this.hostRatingRepository = hostRatingRepository;
+        this.userServiceClient = userServiceClient;
+        this.reservationServiceClient = reservationServiceClient;
+    }
+
+    public MessageResponse rateHost(String host,
+                                    CreateHostRatingRequest createHostRatingRequest,
+                                    String jwtToken) {
+        Integer rating = createHostRatingRequest.getRating();
+        if (rating == null || rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be between 1 and 5.");
+        }
+
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_GUEST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        if (hostRatingRepository.existsByGuestAndHost(userDetails.getUsername(), host)) {
+            throw new IllegalArgumentException("You have already rated this host.");
+        }
+
+        if (reservationServiceClient.isGuestHasSuccessfullyPassedReservationHost(host, jwtToken)) {
+            HostRating newHostRating = new HostRating(
+                    userDetails.getUsername(),
+                    host,
+                    createHostRatingRequest.getRating()
+            );
+
+            newHostRating.setDateTime(LocalDateTime.now());
+
+            hostRatingRepository.save(newHostRating);
+            return new MessageResponse("Host rated successfully.");
+        } else {
+            throw new SecurityException("You cannot rate this host.");
+        }
+    }
+
+    public List<HostRatingDTO> getAllHostRatingsByHost(String host) {
+        return hostRatingRepository.findByHost(host)
+                .stream()
+                .map(HostRatingMapper::toHostRatingDTO)
+                .collect(Collectors.toList());
+    }
+
+    public HostRatingDTO getHostRatingById(UUID hostRatingId) {
+        HostRating hostRating = hostRatingRepository.findById(hostRatingId)
+                .orElseThrow(() -> new NoSuchElementException("Host rating not found with id: " + hostRatingId));
+
+        return HostRatingMapper.toHostRatingDTO(hostRating);
+    }
+
+    public MessageResponse updateHostRating(UUID hostRatingId,
+                                            UpdateHostRatingRequest updateHostRatingRequest,
+                                            String jwtToken) {
+        Integer rating = updateHostRatingRequest.getRating();
+        if (rating == null || rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be between 1 and 5.");
+        }
+
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_GUEST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        HostRating hostRating = hostRatingRepository.findById(hostRatingId)
+                .orElseThrow(() -> new NoSuchElementException("Host rating not found with id: " + hostRatingId));
+
+        if (!Objects.equals(hostRating.getGuest(), userDetails.getUsername())) {
+            throw new SecurityException("User did not create this host rating.");
+        }
+
+        hostRating.setRating(updateHostRatingRequest.getRating());
+        hostRating.setDateTime(LocalDateTime.now());
+
+        hostRatingRepository.save(hostRating);
+        return new MessageResponse("Host rating updated successfully.");
+    }
+
+    public Double getHostAverageRating(String host) {
+        Double averageRating = hostRatingRepository.findAverageRatingByHost(host);
+        if (averageRating == null) {
+            return 0.0;
+        }
+
+        return BigDecimal.valueOf(averageRating)
+                .setScale(1, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+
+    public MessageResponse deleteHostRating(UUID hostRatingId,
+                                            String jwtToken) {
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_GUEST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        HostRating hostRating = hostRatingRepository.findById(hostRatingId)
+                .orElseThrow(() -> new NoSuchElementException("Host rating not found with id: " + hostRatingId));
+
+        if (!Objects.equals(hostRating.getGuest(), userDetails.getUsername())) {
+            throw new SecurityException("User did not create this host rating.");
+        }
+
+        hostRatingRepository.delete(hostRating);
+        return new MessageResponse("Host rating deleted successfully.");
+    }
+}
