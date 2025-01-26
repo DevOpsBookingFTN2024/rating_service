e591cc4b0c641c8a0a8f662207dd4d0c81c9801a
Ruzica74 <ruzicadosenovicc@gmail.com>
2025-01-26 21:27:42 +0100

    feat: Adding rating management (#7)
    Adding rating management
    
    Co-authored-by: Ruzica74 <ruzicaa997@gmail.com>

---
+++ src/main/java/uns/ac/rs/rating_service/controller/AccommodationRatingController.java
@@ -0,0 +1,70 @@
+package uns.ac.rs.rating_service.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uns.ac.rs.rating_service.dto.AccommodationRatingDTO;
+import uns.ac.rs.rating_service.dto.request.CreateAccommodationRatingRequest;
+import uns.ac.rs.rating_service.dto.request.UpdateAccommodationRatingRequest;
+import uns.ac.rs.rating_service.dto.response.MessageResponse;
+import uns.ac.rs.rating_service.service.AccommodationRatingService;
+import java.util.List;
+import java.util.UUID;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/ratings/accommodation")
+public class AccommodationRatingController {
+    @Autowired
+    private AccommodationRatingService accommodationRatingService;
+
+    @PostMapping("/create/{idAccommodation}")
+    public ResponseEntity<?> rateAccommodation(@PathVariable UUID idAccommodation,
+                                               @Valid @RequestBody CreateAccommodationRatingRequest createAccommodationRatingRequest,
+                                               @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = accommodationRatingService.rateAccommodation(
+                idAccommodation, createAccommodationRatingRequest, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @GetMapping("/all/{idAccommodation}")
+    public ResponseEntity<?> getAllAccommodationRatingsByIdAccommodation(@PathVariable UUID idAccommodation) {
+        List<AccommodationRatingDTO> accommodationRatings = accommodationRatingService
+                .getAllAccommodationRatingsByIdAccommodation(idAccommodation);
+        return ResponseEntity.ok(accommodationRatings);
+    }
+
+    @GetMapping("/{accommodationRatingId}")
+    public ResponseEntity<?> getAccommodationRatingById(@PathVariable UUID accommodationRatingId) {
+        AccommodationRatingDTO accommodationRating = accommodationRatingService
+                .getAccommodationRatingById(accommodationRatingId);
+        return ResponseEntity.ok(accommodationRating);
+    }
+
+    @PutMapping("/update/{accommodationRatingId}")
+    public ResponseEntity<?> updateAccommodationRating(@PathVariable UUID accommodationRatingId,
+                                                       @Valid @RequestBody UpdateAccommodationRatingRequest updateAccommodationRatingRequest,
+                                                       @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = accommodationRatingService.updateAccommodationRating(
+                accommodationRatingId, updateAccommodationRatingRequest, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @GetMapping("/average/{idAccommodation}")
+    public ResponseEntity<?> getAccommodationAverageRating(@PathVariable UUID idAccommodation) {
+        Double averageRating = accommodationRatingService.getAccommodationAverageRating(idAccommodation);
+        return ResponseEntity.ok(averageRating);
+    }
+
+    @DeleteMapping("/delete/{accommodationRatingId}")
+    public ResponseEntity<?> deleteAccommodationRating(@PathVariable UUID accommodationRatingId,
+                                                       @RequestHeader("Authorization") String authorizationHeader) {
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        MessageResponse messageResponse = accommodationRatingService
+                .deleteAccommodationRating(accommodationRatingId, jwtToken);
+        return ResponseEntity.ok(messageResponse);
+    }
+}
---
+++ src/main/java/uns/ac/rs/rating_service/service/AccommodationRatingService.java
@@ -0,0 +1,153 @@
+package uns.ac.rs.rating_service.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import uns.ac.rs.rating_service.dto.AccommodationRatingDTO;
+import uns.ac.rs.rating_service.dto.UserDTO;
+import uns.ac.rs.rating_service.dto.request.CreateAccommodationRatingRequest;
+import uns.ac.rs.rating_service.dto.request.UpdateAccommodationRatingRequest;
+import uns.ac.rs.rating_service.dto.response.MessageResponse;
+import uns.ac.rs.rating_service.mapper.AccommodationRatingMapper;
+import uns.ac.rs.rating_service.model.AccommodationRating;
+import uns.ac.rs.rating_service.repository.AccommodationRatingRepository;
+import uns.ac.rs.rating_service.service.client.ReservationServiceClient;
+import uns.ac.rs.rating_service.service.client.UserServiceClient;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class AccommodationRatingService {
+    private final AccommodationRatingRepository accommodationRatingRepository;
+    private final UserServiceClient userServiceClient;
+    private final ReservationServiceClient reservationServiceClient;
+
+    public AccommodationRatingService(AccommodationRatingRepository accommodationRatingRepository,
+                                      UserServiceClient userServiceClient,
+                                      ReservationServiceClient reservationServiceClient) {
+        this.accommodationRatingRepository = accommodationRatingRepository;
+        this.userServiceClient = userServiceClient;
+        this.reservationServiceClient = reservationServiceClient;
+    }
+
+    public MessageResponse rateAccommodation(UUID idAccommodation,
+                                             CreateAccommodationRatingRequest createAccommodationRatingRequest,
+                                             String jwtToken) {
+        Integer rating = createAccommodationRatingRequest.getRating();
+        if (rating == null || rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be between 1 and 5.");
+        }
+
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_GUEST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        if (accommodationRatingRepository.existsByGuestAndIdAccommodation(userDetails.getUsername(), idAccommodation)) {
+            throw new IllegalArgumentException("You have already rated this accommodation.");
+        }
+
+        if (reservationServiceClient.isGuestHasSuccessfullyPassedReservationAccommodation(idAccommodation, jwtToken)) {
+            AccommodationRating newAccommodationRating = new AccommodationRating(
+                    userDetails.getUsername(),
+                    idAccommodation,
+                    createAccommodationRatingRequest.getRating()
+            );
+
+            newAccommodationRating.setDateTime(LocalDateTime.now());
+
+            accommodationRatingRepository.save(newAccommodationRating);
+            return new MessageResponse("Accommodation rated successfully.");
+        } else {
+            throw new SecurityException("You cannot rate this accommodation.");
+        }
+    }
+
+    public List<AccommodationRatingDTO> getAllAccommodationRatingsByIdAccommodation(UUID idAccommodation) {
+        return accommodationRatingRepository.findByIdAccommodation(idAccommodation)
+                .stream()
+                .map(AccommodationRatingMapper::toAccommodationRatingDTO)
+                .collect(Collectors.toList());
+    }
+
+    public AccommodationRatingDTO getAccommodationRatingById(UUID accommodationRatingId) {
+        AccommodationRating accommodationRating = accommodationRatingRepository.findById(accommodationRatingId)
+                .orElseThrow(() -> new NoSuchElementException("Accommodation rating not found with id: "
+                        + accommodationRatingId));
+
+        return AccommodationRatingMapper.toAccommodationRatingDTO(accommodationRating);
+    }
+
+    public MessageResponse updateAccommodationRating(UUID accommodationRatingId,
+                                                     UpdateAccommodationRatingRequest updateAccommodationRatingRequest,
+                                                     String jwtToken) {
+        Integer rating = updateAccommodationRatingRequest.getRating();
+        if (rating == null || rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be between 1 and 5.");
+        }
+
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_GUEST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        AccommodationRating accommodationRating = accommodationRatingRepository.findById(accommodationRatingId)
+                .orElseThrow(() -> new NoSuchElementException("Accommodation rating not found with id: "
+                        + accommodationRatingId));
+
+        if (!Objects.equals(accommodationRating.getGuest(), userDetails.getUsername())) {
+            throw new SecurityException("User did not create this accommodation rating.");
+        }
+
+        accommodationRating.setRating(updateAccommodationRatingRequest.getRating());
+        accommodationRating.setDateTime(LocalDateTime.now());
+
+        accommodationRatingRepository.save(accommodationRating);
+        return new MessageResponse("Accommodation rating updated successfully.");
+    }
+
+    public Double getAccommodationAverageRating(UUID idAccommodation) {
+        Double averageRating = accommodationRatingRepository.findAverageRatingByIdAccommodation(idAccommodation);
+        if (averageRating == null) {
+            return 0.0;
+        }
+
+        return BigDecimal.valueOf(averageRating)
+                .setScale(1, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+
+    public MessageResponse deleteAccommodationRating(UUID accommodationRatingId,
+                                                     String jwtToken) {
+        UserDTO userDetails = userServiceClient.getUserDetails(jwtToken);
+        if (userDetails == null) {
+            throw new IllegalStateException("User details could not be retrieved.");
+        }
+        if (!userDetails.getRoles().contains("ROLE_GUEST")) {
+            throw new SecurityException("User do not have permission for this action.");
+        }
+
+        AccommodationRating accommodationRating = accommodationRatingRepository.findById(accommodationRatingId)
+                .orElseThrow(() -> new NoSuchElementException("Accommodation rating not found with id: "
+                        + accommodationRatingId));
+
+        if (!Objects.equals(accommodationRating.getGuest(), userDetails.getUsername())) {
+            throw new SecurityException("User did not create this accommodation rating.");
+        }
+
+        accommodationRatingRepository.delete(accommodationRating);
+        return new MessageResponse("Accommodation rating deleted successfully.");
+    }
+}
