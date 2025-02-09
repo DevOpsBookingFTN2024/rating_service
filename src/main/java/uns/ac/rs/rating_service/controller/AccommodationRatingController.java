ae9e7c804687321b72f2c9b307fc04fd6d48152e
Nikola Jovic <jovnikola99@gmail.com>
2025-02-09 20:30:53 +0100

    fix: Add logging to controllers (#17)

--- src/main/java/uns/ac/rs/rating_service/controller/AccommodationRatingController.java
+++ src/main/java/uns/ac/rs/rating_service/controller/AccommodationRatingController.java
@@ -1,6 +1,7 @@
 package uns.ac.rs.rating_service.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/ratings/accommodation")
 public class AccommodationRatingController {
@@ -24,22 +26,28 @@ public class AccommodationRatingController {
                              @Valid @RequestBody CreateAccommodationRatingRequest createAccommodationRatingRequest,
                              @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Received request to rate accommodation with ID: {}", idAccommodation);
         MessageResponse messageResponse = accommodationRatingService
                 .rateAccommodation(idAccommodation, createAccommodationRatingRequest, jwtToken);
+        log.info("Successfully rated accommodation with ID: {}", idAccommodation);
         return ResponseEntity.ok(messageResponse);
     }
 
     @GetMapping("/all/{idAccommodation}")
     public ResponseEntity<?> getAllAccommodationRatingsByIdAccommodation(@PathVariable UUID idAccommodation) {
+        log.info("Fetching all ratings for accommodation ID: {}", idAccommodation);
         List<AccommodationRatingDTO> accommodationRatings = accommodationRatingService
                 .getAllAccommodationRatingsByIdAccommodation(idAccommodation);
+        log.info("Fetched {} ratings for accommodation ID: {}", accommodationRatings.size(), idAccommodation);
         return ResponseEntity.ok(accommodationRatings);
     }
 
     @GetMapping("/{accommodationRatingId}")
     public ResponseEntity<?> getAccommodationRatingById(@PathVariable UUID accommodationRatingId) {
+        log.info("Fetching rating with ID: {}", accommodationRatingId);
         AccommodationRatingDTO accommodationRating = accommodationRatingService
                 .getAccommodationRatingById(accommodationRatingId);
+        log.info("Successfully fetched rating with ID: {}", accommodationRatingId);
         return ResponseEntity.ok(accommodationRating);
     }
 
@@ -47,8 +55,10 @@ public class AccommodationRatingController {
     public ResponseEntity<?> getAccommodationRatingByGuest(@PathVariable UUID accommodationRatingId,
                                                            @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Fetching guest-specific rating for ID: {}", accommodationRatingId);
         AccommodationRatingDTO accommodationRating = accommodationRatingService
                 .getAccommodationRatingByGuest(jwtToken, accommodationRatingId);
+        log.info("Successfully fetched guest-specific rating for ID: {}", accommodationRatingId);
         return ResponseEntity.ok(accommodationRating);
     }
 
@@ -57,14 +67,18 @@ public class AccommodationRatingController {
                              @Valid @RequestBody UpdateAccommodationRatingRequest updateAccommodationRatingRequest,
                              @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Updating rating with ID: {}", accommodationRatingId);
         MessageResponse messageResponse = accommodationRatingService
                 .updateAccommodationRating(accommodationRatingId, updateAccommodationRatingRequest, jwtToken);
+        log.info("Successfully updated rating with ID: {}", accommodationRatingId);
         return ResponseEntity.ok(messageResponse);
     }
 
     @GetMapping("/average/{idAccommodation}")
     public ResponseEntity<?> getAccommodationAverageRating(@PathVariable UUID idAccommodation) {
+        log.info("Calculating average rating for accommodation with ID: {}", idAccommodation);
         Double averageRating = accommodationRatingService.getAccommodationAverageRating(idAccommodation);
+        log.info("Average rating for accommodation with ID {} is: {}", idAccommodation, averageRating);
         return ResponseEntity.ok(averageRating);
     }
 
@@ -72,8 +86,10 @@ public class AccommodationRatingController {
     public ResponseEntity<?> deleteAccommodationRating(@PathVariable UUID accommodationRatingId,
                                                        @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Deleting rating with ID: {}", accommodationRatingId);
         MessageResponse messageResponse = accommodationRatingService
                 .deleteAccommodationRating(accommodationRatingId, jwtToken);
+        log.info("Successfully deleted rating with ID: {}", accommodationRatingId);
         return ResponseEntity.ok(messageResponse);
     }
 }
--- src/main/java/uns/ac/rs/rating_service/controller/HostRatingController.java
+++ src/main/java/uns/ac/rs/rating_service/controller/HostRatingController.java
@@ -1,6 +1,7 @@
 package uns.ac.rs.rating_service.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
+@Slf4j
 @RestController
 @RequestMapping("/ratings/host")
 public class HostRatingController {
@@ -24,20 +26,26 @@ public class HostRatingController {
                                       @Valid @RequestBody CreateHostRatingRequest createHostRatingRequest,
                                       @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Received request to rate host {}", host);
         MessageResponse messageResponse = hostRatingService
                 .rateHost(host, createHostRatingRequest, jwtToken);
+        log.info("Successfully rated rate host {}", host);
         return ResponseEntity.ok(messageResponse);
     }
 
     @GetMapping("/all/{host}")
     public ResponseEntity<?> getAllHostRatingsByHost(@PathVariable String host) {
+        log.info("Fetching all ratings for host {}", host);
         List<HostRatingDTO> hostRatings = hostRatingService.getAllHostRatingsByHost(host);
+        log.info("Fetched {} ratings for host {}", hostRatings.size(), host);
         return ResponseEntity.ok(hostRatings);
     }
 
     @GetMapping("/{hostRatingId}")
     public ResponseEntity<?> getHostRatingById(@PathVariable UUID hostRatingId) {
+        log.info("Fetching rating with ID: {}", hostRatingId);
         HostRatingDTO hostRating = hostRatingService.getHostRatingById(hostRatingId);
+        log.info("Successfully fetched rating with ID: {}", hostRatingId);
         return ResponseEntity.ok(hostRating);
     }
 
@@ -45,7 +53,9 @@ public class HostRatingController {
     public ResponseEntity<?> getHostRatingByGuest(@PathVariable String host,
                                                   @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Fetching guest-specific rating for host {}", host);
         HostRatingDTO hostRating = hostRatingService.getHostRatingByGuest(jwtToken, host);
+        log.info("Successfully fetched guest-specific rating for host {}", host);
         return ResponseEntity.ok(hostRating);
     }
 
@@ -54,14 +64,18 @@ public class HostRatingController {
                                               @Valid @RequestBody UpdateHostRatingRequest updateHostRatingRequest,
                                               @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Updating rating with ID: {}", hostRatingId);
         MessageResponse messageResponse = hostRatingService
                 .updateHostRating(hostRatingId, updateHostRatingRequest, jwtToken);
+        log.info("Successfully updated rating with ID: {}", hostRatingId);
         return ResponseEntity.ok(messageResponse);
     }
 
     @GetMapping("/average/{host}")
     public ResponseEntity<?> getHostAverageRating(@PathVariable String host) {
+        log.info("Calculating average rating for host {}", host);
         Double averageRating = hostRatingService.getHostAverageRating(host);
+        log.info("Average rating for host {} is: {}", host, averageRating);
         return ResponseEntity.ok(averageRating);
     }
 
@@ -69,7 +83,9 @@ public class HostRatingController {
     public ResponseEntity<?> deleteHostRating(@PathVariable UUID hostRatingId,
                                               @RequestHeader("Authorization") String authorizationHeader) {
         String jwtToken = authorizationHeader.replace("Bearer ", "");
+        log.info("Deleting rating with ID: {}", hostRatingId);
         MessageResponse messageResponse = hostRatingService.deleteHostRating(hostRatingId, jwtToken);
+        log.info("Successfully deleted rating with ID: {}", hostRatingId);
         return ResponseEntity.ok(messageResponse);
     }
 }
